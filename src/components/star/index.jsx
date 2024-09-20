4eef5394ded6a96604cb3810d9e044108bf75e21
Onkar Srivastava <onkarsrivastava@Onkars-MacBook-Pro.local>
2024-09-20 15:36:44 +0530

    Star rating feedback using react concepts

---
+++ src/components/star/index.jsx
@@ -0,0 +1,11 @@
+export default function StarRating(noOfStars) {
+
+    return <div className="star-rating">
+            {
+               [...Array(noOfStars)].map((_,index)=>{
+
+                
+               })
+            }
+    </div>
+}
