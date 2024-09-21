6855da00b1d5090dd9531c84ca5a0bc2aebdc317
Onkar Srivastava <onkarsrivastava@Onkars-MacBook-Pro.local>
2024-09-21 12:15:11 +0530

    made functional changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-
+import StarRating from './components/star';
 function App() {
   
+    return(
+        <div className='App'>
+            <StarRating noOfStars={10}/>
+
+            </div>
+    );
 }
 
 export default App;
